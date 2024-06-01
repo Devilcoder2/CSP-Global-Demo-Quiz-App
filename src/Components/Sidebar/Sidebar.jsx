8a43eaa4ff460585093b18b1646cb6620b870e12
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-02 02:05:07 +0530

    Implemented the routes changes on button clicked

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,21 +1,43 @@
 import FeedIcon from "@mui/icons-material/Feed";
 import SchoolOutlinedIcon from "@mui/icons-material/SchoolOutlined";
 import PersonIcon from "@mui/icons-material/Person";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const clickHandler = (url) => {
+    navigate(url);
+  };
+
   return (
     <div>
       <div className="h-[100vh] w-72 bg-white">
         <div className="text-gray-500 text-2xl font-bold">LOGO</div>
-        <div>
+        <div
+          onClick={() => {
+            clickHandler("/papers");
+          }}
+          className="hover:cursor-pointer"
+        >
           <FeedIcon />
           Paper
         </div>
-        <div>
+        <div
+          onClick={() => {
+            clickHandler("/test");
+          }}
+          className="hover:cursor-pointer"
+        >
           <SchoolOutlinedIcon />
           Practice Test
         </div>
-        <div>
+        <div
+          onClick={() => {
+            clickHandler("/user");
+          }}
+          className="hover:cursor-pointer"
+        >
           <PersonIcon />
           User
         </div>
