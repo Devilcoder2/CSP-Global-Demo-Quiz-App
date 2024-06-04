cd83480452d1f352c32cd1c161b3a14feb928e99
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-04 13:55:53 +0530

    Implemented heading & subheading of the practice test

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -1,5 +1,23 @@
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <div className="text-[#DBDBEB] mt-6 ml-4">
+      <div>
+        <h1 className="text-3xl">Free Practice Test</h1>
+        <div className=" flex mt-3">
+          <h1 className="text-xl font-semibold hover:underline hover:cursor-pointer">
+            Sneat
+          </h1>
+          <span className="ml-10">
+            <ul className=" list-disc">
+              <li className="text-xl opacity-50">
+                New practice test each time
+              </li>
+            </ul>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Test;
