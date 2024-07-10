e733c44a962eb9a7da5cccbf39fdfd39234dd4cb
Akash Yadav <idoutofbox@gmail.com>
2024-07-10 21:24:52 +0530

    Add Questions page

--- src/Components/AllQuestions/AllQuestions.jsx
+++ src/Components/AllQuestions/AllQuestions.jsx
@@ -49,13 +49,96 @@ const AllQuestions = () => {
     }
     setLoading(false);
   };
-  const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
-    backgroundColor: theme.palette.background.default,
-    borderRadius: theme.shape.borderRadius,
-    "& .MuiAccordionSummary-content": {
-      display: "flex",
-      justifyContent: "space-between",
-      alignItems: "center",
+  // const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
+  //   backgroundColor: theme.palette.background.default,
+  //   borderRadius: theme.shape.borderRadius,
+  //   "& .MuiAccordionSummary-content": {
+  //     display: "flex",
+  //     justifyContent: "space-between",
+  //     alignItems: "center",
+  //   },
+  // }));
+  const CustomAutocomplete = styled(Autocomplete)(() => ({
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "#DBDBEB",
+        opacity: "50%",
+        borderRadius: "12px",
+      },
+      "&:hover fieldset": {
+        borderColor: "white",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#696CFF",
+      },
+    },
+    "& .MuiAutocomplete-popupIndicator": {
+      color: "#DBDBEB",
+    },
+    "& .MuiAutocomplete-popupIndicator.Mui-focused": {
+      color: "#696CFF",
+    },
+    "& .MuiAutocomplete-listbox": {
+      backgroundColor: "#2B2C40",
+      color: "#DBDBEB",
+    },
+  }));
+
+  const selectStyles = {
+    "& .MuiInputLabel-root": {
+      color: "#DBDBEB",
+      opacity: "50%",
+      fontSize: "16px",
+    },
+    "& .MuiInputLabel-root.Mui-focused": {
+      color: "#696CFF",
+      opacity: "100%",
+      fontSize: "16px",
+    },
+    "& .MuiInputBase-input": {
+      color: "#DBDBEB",
+    },
+    "& .MuiInputBase-input.Mui-focused": {
+      color: "#696CFF",
+      opacity: "100%",
+    },
+    width: "320px",
+  };
+  const StyledCard = styled(Card)(() => ({
+    margin: "20px 0",
+    padding: "10px",
+    backgroundColor: "rgba(35, 35, 51, 0.9)",
+    color: "rgb(255, 255, 255)",
+    boxShadow:
+      "rgba(145, 158, 171, 0.24) 0px 0px 2px 0px, rgba(145, 158, 171, 0.24) 0px 16px 32px -4px",
+    borderRadius: "16px",
+  }));
+
+  const StyledAccordionSummary = styled(AccordionSummary)(() => ({
+    backgroundColor: "rgba(35, 35, 51, 0.9)",
+    color: "#DBDBEB",
+    "& .MuiAccordionSummary-expandIconWrapper": {
+      color: "#DBDBEB",
+    },
+  }));
+
+  const StyledAccordionDetails = styled(AccordionDetails)(() => ({
+    backgroundColor: "rgba(35, 35, 51, 0.9)",
+    color: "#DBDBEB",
+  }));
+
+  const StyledTypography = styled(Typography)(() => ({
+    fontSize: "20px",
+  }));
+
+  const StyledLink = styled("a")(() => ({
+    color: "#696CFF",
+    wordBreak: "break-all",
+  }));
+  const StyledButton = styled(Button)(() => ({
+    backgroundColor: "rgb(105, 108, 255)",
+    "&:hover": {
+      backgroundColor: "rgba(105, 108, 255, 0.8)",
     },
   }));
   return (
@@ -140,8 +223,8 @@ const AllQuestions = () => {
               width: "100%",
               maxWidth: "1200px",
               padding: "16px",
-              backgroundColor: "rgb(255, 255, 255)",
-              color: "rgb(33, 43, 54)",
+              backgroundColor: "rgba(35, 35, 51, 0.9)",
+              color: "rgb(255, 255, 255)",
               transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
               backgroundImage: "none",
               overflow: "hidden",
@@ -152,9 +235,8 @@ const AllQuestions = () => {
               zIndex: 0,
             }}
           >
-            {/* <Box sx={{ display: "flex", gap: "20px" }}> */}
-            <Autocomplete
-              sx={{ flex: 1 }}
+            <CustomAutocomplete
+              sx={selectStyles}
               options={providers}
               value={provider}
               onChange={(e, val) => {
@@ -170,8 +252,8 @@ const AllQuestions = () => {
                 />
               )}
             />
-            <Autocomplete
-              sx={{ flex: 1 }}
+            <CustomAutocomplete
+              sx={selectStyles}
               options={exams[provider] || []}
               value={exam}
               onChange={(e, val) => {
@@ -186,120 +268,98 @@ const AllQuestions = () => {
                 />
               )}
             />
-            {/* </Box> */}
-            <Button
+            <StyledButton
               variant="contained"
               onClick={handleFetchQuestions}
-              color="primary"
               disabled={!provider || !exam}
             >
               Fetch Questions
-            </Button>
+            </StyledButton>
           </Card>
         </Box>
       </Container>
 
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        questions.map((q) => (
-          <Card
-            key={q.id}
-            elevation={3}
-            variation="outlined"
-            style={{ margin: "20px 0", padding: "10px" }}
-          >
-            <CardContent>
-              <div style={{ display: "flex", justifyContent: "space-between" }}>
-                <Typography variant="h5" style={{ fontsize: "2 rem" }}>
-                  Question:{q.id}
-                </Typography>
-              </div>
-              <Typography variant="body1" style={{ margin: "10px 0" }}>
-                {q.question}
-              </Typography>
-              {q.img && (
-                <img
-                  src={q.img}
-                  alt="question"
-                  className="img-question"
-                  style={{ width: "100%", marginBottom: "10px" }}
-                />
-              )}
-              <ul style={{ listStyleType: "none" }}>
-                {q.options.map((option, index) => (
-                  <li key={index} ClassName="ind-opt">
-                    <span ClassName="mcq-opt">{option}</span>
-                  </li>
-                ))}
-              </ul>
-              <Accordion>
-                <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
-                  <Typography
-                    variant="subtitle1"
-                    sx={{ display: "flex", alignItems: "center" }}
-                  >
-                    <ConstructionOutlinedIcon sx={{ marginRight: "8px" }} />
-                    Show Solution
-                  </Typography>
-                </StyledAccordionSummary>
-                <AccordionDetails>
-                  <Typography>Answer: {q.answer}</Typography>
-                  <Typography variant="body2" style={{ marginTop: "15px" }}>
-                    Explanation: {q.explanation}
-                  </Typography>
-                  {q.answerImage && (
-                    <img
-                      src={q.answerImage}
-                      alt="Answer"
-                      style={{ width: "100%", marginTop: "10px" }}
-                    />
-                  )}
-                  <Typography variant="body2" style={{ marginTop: "15px" }}>
-                    Reference:{" "}
-                    <a
-                      href={q.reference}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ wordBreak: "break-all" }}
+      <Container>
+        <Box sx={{ marginTop: "60px", marginBottom: "20px" }}>
+          {loading
+            ? null
+            : questions.map((q) => (
+                <StyledCard key={q.id} elevation={3} variant="outlined">
+                  <CardContent>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                      }}
                     >
-                      {q.reference}
-                    </a>
-                  </Typography>
-                </AccordionDetails>
-              </Accordion>
-
-              {/* <Accordion>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />} />
-                <AccordionDetails>
-                  <Typography>Answer: {q.answer}</Typography>
-                  <Typography variant="body2" style={{ marginTop: "15px" }}>
-                    Explanation: {q.explanation}
-                  </Typography>
-                  {q.answerImage && (
-                    <img
-                      src={q.answerImage}
-                      alt="Answer"
-                      style={{ width: "100%", marginTop: "10px" }}
-                    />
-                  )}
-                  <Typography variant="body2" style={{ marginTop: "15px" }}>
-                    Reference:{" "}
-                    <a
-                      href={q.reference}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      style={{ wordBreak: "break-all" }}
-                    >
-                      {q.reference}
-                    </a>
-                  </Typography>
-                </AccordionDetails>
-              </Accordion> */}
-            </CardContent>
-          </Card>
-        ))
-      )}
+                      <StyledTypography variant="h5">
+                        Question: {q.id}
+                      </StyledTypography>
+                    </div>
+                    <Typography variant="body1" style={{ margin: "10px 0" }}>
+                      {q.question}
+                    </Typography>
+                    {q.img && (
+                      <img
+                        src={q.img}
+                        alt="question"
+                        style={{ width: "100%", marginBottom: "10px" }}
+                      />
+                    )}
+                    <ul style={{ listStyleType: "none" }}>
+                      {q.options.map((option, index) => (
+                        <li key={index} style={{ margin: "5px 0" }}>
+                          <span>{option}</span>
+                        </li>
+                      ))}
+                    </ul>
+                    <Accordion>
+                      <StyledAccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography
+                          variant="subtitle1"
+                          sx={{ display: "flex", alignItems: "center" }}
+                        >
+                          <ConstructionOutlinedIcon
+                            sx={{ marginRight: "8px" }}
+                          />
+                          Show Solution
+                        </Typography>
+                      </StyledAccordionSummary>
+                      <StyledAccordionDetails>
+                        <Typography>Answer: {q.answer}</Typography>
+                        <Typography
+                          variant="body2"
+                          style={{ marginTop: "15px" }}
+                        >
+                          Explanation: {q.explanation}
+                        </Typography>
+                        {q.answerImage && (
+                          <img
+                            src={q.answerImage}
+                            alt="Answer"
+                            style={{ width: "100%", marginTop: "10px" }}
+                          />
+                        )}
+                        <Typography
+                          variant="body2"
+                          style={{ marginTop: "15px" }}
+                        >
+                          Reference:{" "}
+                          <StyledLink
+                            href={q.reference}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            {q.reference}
+                          </StyledLink>
+                        </Typography>
+                      </StyledAccordionDetails>
+                    </Accordion>
+                  </CardContent>
+                </StyledCard>
+              ))}
+        </Box>
+      </Container>
     </Container>
   );
 };
